d25902437357050da2bb90be590171dcc09ce310
Kaycee Nuel <heykayceee@gmail.com>
2025-06-03 20:47:22 +0100

    implementing server api

--- main.go
+++ main.go
@@ -1,27 +1,30 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
+// image struct represents data about an image metadata
+type Image struct {
+	Title   string `json: "title"`
+	AltText string `json: "alt_text"`
+	URL     string `json: "url"`
+}
+
+// initialising image data 
+images := []Image{
+    {
+      title: "Sunset",
+      alt_text: "Clouds at sunset",
+      url: "https://images.unsplash.com/photo-1506815444479-bfdb1e96c566?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    },
+    {
+      title: "Mountain",
+      alt_text: "A mountain at sunset",
+      url: "https://images.unsplash.com/photo-1540979388789-6cee28a1cdc9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    },
+}
+
 func main() {
-	// Define the static JSON data
-	data := map[string]interface{}{
-		"message": "Hello, World!",
-		"status":  "success",
-		"data": map[string]string{
-			"author": "John Doe",
-			"year":   "2025",
-		},
-	}
-
-	// HTTP handler function
-	http.HandleFunc("/json", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(data)
-	})
-
-	// Start the HTTP server
-	http.ListenAndServe(":8080", nil)
+	
 }
